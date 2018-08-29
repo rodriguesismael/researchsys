5880665e477e9239a5ace15952d5ff42df15c0ae
Ismael Rodrigues <ismael@ic.unicamp.br>
2018-08-28 22:11:46 -0300

    Alteracoes no menu

--- app/controllers/GeneralPagesController.php
+++ app/controllers/GeneralPagesController.php
@@ -1,6 +1,7 @@
 <?php
 class GeneralPagesController extends Controller{
 	public function infoPage(){
+		$this->f3->set('sobre','active');
 		$this->f3->set('content','informacoes.html');
 		echo \Template::instance()->render('tela.htm');
 	}
@@ -9,12 +10,14 @@ class GeneralPagesController extends Controller{
 		$universidade = new UniversidadeDAO();
 		$universidades = $universidade->getList();
 		unset($universidade);
+		$this->f3->set('sobre','active');
 		$this->f3->set('universidades',$universidades);
 		$this->f3->set('content','sobreUniversidades.html');
 		echo \Template::instance()->render('tela.htm');
 	}
 
 	public function contatoPage(){
+		$this->f3->set('contato','active');
 		$this->f3->set('content','contato.html');
 		echo \Template::instance()->render('tela.htm');
 	}	
