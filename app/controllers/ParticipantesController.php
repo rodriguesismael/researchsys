fba7cc68be52eb63a185b05fa20f8f60b864686b
Ismael Rodrigues <ismael@ic.unicamp.br>
2018-11-05 22:46:03 -0200

    Atualizacao msg convite

--- app/controllers/ParticipantesController.php
+++ app/controllers/ParticipantesController.php
@@ -68,7 +68,7 @@ class ParticipantesController extends Controller{
 	function convidar(){
 		$this->isAdmin();
 		$obs="<strong>OBSERVAÇÃO: O sistema está em período de testes. Ao preencher os dados demográficos, favor acrescentar 'Teste' por útimo no campo nome. 
-				Após finalizar o processo, favor elaborar um texto com impressões e/ou problemas encontrados bem como sugestões de melhorias. Encaminhar para isrodrigues5@gmail.com 
+				Após finalizar o processo, favor elaborar um texto com impressões e/ou problemas encontrados bem como sugestões de melhorias. Encaminhar para isrodrigues5@gmail.com até 13 de novembro. 
 				com o assunto \"Testes Autorregular\".</strong>";
 		$link="";
 		$msgEmail = "<h4>Prezado(a) aluno(a),</h4>
